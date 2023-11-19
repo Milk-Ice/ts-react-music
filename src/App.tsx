3f0c3fe0c0f07aa6e02b9bb0d902b9a5750e5645
Milk-Ice <mathila1218@gmail.com>
2023-11-19 13:17:49 +0800

    feat(router): 0_项目搭建-路由集成-路由的基本配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import routes from './router'
 function App() {
-  return <div className="App">App</div>
+  return <div className="App">{useRoutes(routes)}</div>
 }
 
 export default App
