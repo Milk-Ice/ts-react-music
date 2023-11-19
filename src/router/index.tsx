1a6a4a52a75d4c8f91b45be5f7f3f1d446eb55e4
Milk-Ice <mathila1218@gmail.com>
2023-11-19 15:30:53 +0800

    perf(lazy): 0_项目搭建-路由懒加载的组件分包

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import Discover from '@/views/discover'
+import React, { lazy } from 'react'
 import { Navigate, type RouteObject } from 'react-router-dom'
-import Mine from '@/views/mine'
+
+// import Discover from '@/views/discover'
+/* import Mine from '@/views/mine'
 import Focus from '@/views/focus'
-import Download from '@/views/download'
+import Download from '@/views/download' */
+
+const Discover = lazy(() => import('@/views/discover'))
+const Mine = lazy(() => import('@/views/mine'))
+const Focus = lazy(() => import('@/views/focus'))
+const Download = lazy(() => import('@/views/download'))
+
 const routes: RouteObject[] = [
   {
     path: '/',
